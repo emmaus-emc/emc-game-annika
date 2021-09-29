5a97ba7c7fd485b3a729d080a0e4ff079c12dddf
AnnikaMG <74548149+AnnikaMG@users.noreply.github.com>
2021-09-29 13:45:46 +0000

    speler

--- script.js
+++ script.js
@@ -56,10 +56,10 @@ var tekenAlles = function () {
   // kogel
 
   // speler
-  fill("white");
+  fill("fuchsia");
   rect(spelerX - 25, spelerY - 25, 50, 50);
-  fill("black");
-  ellipse(spelerX, spelerY, 10, 10);
+  fill("purple");
+  ellipse(spelerX, spelerY, 15, 15);
 
   // punten en health
 
@@ -87,7 +87,7 @@ function setup() {
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('purple');
+  background('cyan');
 }
 
 /**
